3895a2d3c226a97f19faab130a41cf9e8f1ff0ce
JinLiye <3211498596@qq.com>
2026-01-12 21:51:47 +0800

    修改导航栏索引策略

---
+++ src/components/LightDarkSwitch.island.ts
@@ -0,0 +1,6 @@
+import LightDarkSwitch from "./LightDarkSwitch.svelte";
+
+const Component: any = LightDarkSwitch;
+
+export default Component;
+
--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
+	name: "JinLiye",
 	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	links: [
 		{
